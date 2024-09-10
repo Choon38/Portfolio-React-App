5baadb30598c6e5bdf6085e17f2ee44cc2a76690
Sneha Kumari <96916394+Choon38@users.noreply.github.com>
2024-09-10 15:05:56 +0530

    Update Projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -36,9 +36,7 @@ const Projects = () => {
           </div>
         </div>
         <p className="mt-10 mb-10">
-          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
-          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
-          purus nec eget eleifend ut elit.
+      
         </p>
       </motion.div>
 
